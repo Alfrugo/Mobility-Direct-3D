e122258a6d58b262b18aba0584322dfe24fc9710
Alfrugo <diez_alfredo@hotmail.com>
2023-03-24 12:32:44 -0500

    buttons animate

--- script/gsap.js
+++ script/gsap.js
@@ -2,7 +2,23 @@
 gsap.registerPlugin(ScrollTrigger)
 
 gsap.from(".requestDemo1", {
-    x: 400,
-    duration: 0.5,
+    y: 200,
+    alpha: 0,
+    duration: 0.7,
     scrollTrigger: ".ulEfficiencies"
+})
+
+
+gsap.from(".requestDemo2", {
+    y: 200,
+    alpha: 0,
+    duration: 0.7,
+    scrollTrigger: "h3"
+})
+
+gsap.from(".requestDemo3", {
+    x: -200,
+    alpha: 0,
+    duration: 0.7,
+    scrollTrigger: ".transformingFac"
 })
