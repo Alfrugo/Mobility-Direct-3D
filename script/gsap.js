06640f455e5bb3370d7b1ee3a9d7c7734c5e057e
Alfrugo <diez_alfredo@hotmail.com>
2023-03-27 11:40:21 -0500

    all gsap done

--- script/gsap.js
+++ script/gsap.js
@@ -8,15 +8,70 @@ gsap.from(".hero", {
     y: -200,
     alpha: 0,   
     duration: 0.7,
-    delay: 0.5,
 })
 
 gsap.from(".heroImage", {
     x: 10,
     y: 100,
     alpha: 0,   
-    duration: 0.7,
+    duration: 1,
+
     
+})
+
+gsap.from("h1", {
+    x: 10,
+    y: 100,
+    alpha: 0,   
+    duration: 0.7,
+    scrollTrigger: ".orangeEmpower",
+    delay: 1,
+})
+
+gsap.from("h3", {
+    x: 10,
+    y: 100,
+    alpha: 0,   
+    duration: 0.7,
+    scrollTrigger: ".mobilityDirectBasic",
+    delay: 1
+   
+})
+
+
+
+gsap.from(".greenTitle", {
+    x: 10,
+    y: 100,
+    alpha: 0,   
+    duration: 0.7,
+    scrollTrigger: "li",
+    delay: 0.74,
+   
+})
+
+gsap.from(".tierDescriptionSimple", {
+    y: 50,
+    alpha: 0,   
+    duration: 0.7,
+    delay: 0.74,
+    scrollTrigger: "h5"
+})
+
+gsap.from(".tierDescriptionPlus", {
+    y: 50,
+    alpha: 0,   
+    duration: 0.7,
+    delay: 1,
+    scrollTrigger: "h5"
+})
+
+gsap.from(".tierDescriptionPro", {
+    y: 50,
+    alpha: 0,   
+    duration: 0.7,
+    delay: 1.3,
+    scrollTrigger: "h5"
 })
 
 gsap.from(".requestDemo1", {
@@ -25,7 +80,6 @@ gsap.from(".requestDemo1", {
     duration: 0.7,
     scrollTrigger: ".ulEfficiencies"
 })
-
 
 gsap.from(".requestDemo2", {
     y: 200,
@@ -39,4 +93,13 @@ gsap.from(".requestDemo3", {
     alpha: 0,
     duration: 0.7,
     scrollTrigger: ".transformingFac"
+})
+
+
+gsap.from(".facilityRender", {
+    scale: 0.5,
+    alpha: 0,
+    duration: 0.7,
+    scrollTrigger: ".facilityRender",
+    delay: 1
 })
